bd4135a6165ad1ec7d8ef7aee33e108fa24a570a
Dokas006 <belop065@gmail.com>
2026-05-17 13:14:01 -0300

    Corrigindo erros de leitura
    Corrigi pequenos erros de organização para facilitar a leitura.

--- src/Test/OrganizadorDeMesaTest.java
+++ src/Test/OrganizadorDeMesaTest.java
@@ -8,15 +8,17 @@ import Serviços.MostrarPedidoMaisCaro;
 
 public class OrganizadorDeMesaTest {
     static void main() {
-        Mesa mesa01 = new Mesa("Pedro Belo Nogueira",101,60);
-        Mesa mesa02 = new Mesa("Pedro Belo Nogueira",101,40);
-        Mesa[] pedidos = {mesa01, mesa02};
+        Mesa mesa01 = new Mesa("Cliente 01",101,60);
+        Mesa mesa02 = new Mesa("Cliente 02",102,80);
+        Mesa mesa03 = new Mesa("Cliente 03",103,20);
+        Mesa[] pedidos = {mesa01, mesa02, mesa03};
         CalcularValorTotal mostrarValorTotal = new CalcularValorTotal();
         MostrarTodasAsMesas mostrarTodasAsMesas = new MostrarTodasAsMesas();
         MostrarPedidoMaisCaro mostrarPedidoMaisCaro = new MostrarPedidoMaisCaro();
         mostrarTodasAsMesas.MostrarTodasAsMesas(pedidos);
-
+        System.out.println("--------------------------------------");
         mostrarValorTotal.MostrarValorTotal(pedidos);
+        System.out.println("--------------------------------------");
         mostrarPedidoMaisCaro.MostrarPedidoMaisCaro(pedidos);
 
     }
